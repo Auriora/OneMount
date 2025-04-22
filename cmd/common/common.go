a91524a1a5135b1fe2fc7327b1949f33ce35a3d1
bcherrington <bruce.cherrington@outlook.com>
2025-04-22 22:25:08 +0200

    Extract xdgVolumeInfo function to common package

--- cmd/common/common.go
+++ cmd/common/common.go
@@ -2,11 +2,15 @@
 package common
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
 	"regexp"
+	"strings"
 
+	"github.com/jstaf/onedriver/fs"
+	"github.com/jstaf/onedriver/fs/graph"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -61,4 +65,35 @@ func GetXDGVolumeInfoName(path string) (string, error) {
 		return "", errors.New("could not find \"Name=\" key")
 	}
 	return name[5:], nil
+}
+
+// CreateXDGVolumeInfo creates .xdg-volume-info for a nice little onedrive logo in the
+// corner of the mountpoint and shows the account name in the nautilus sidebar
+func CreateXDGVolumeInfo(filesystem *fs.Filesystem, auth *graph.Auth) {
+	if child, _ := filesystem.GetPath("/.xdg-volume-info", auth); child != nil {
+		return
+	}
+	log.Info().Msg("Creating .xdg-volume-info")
+	user, err := graph.GetUser(auth)
+	if err != nil {
+		log.Error().Err(err).Msg("Could not create .xdg-volume-info")
+		return
+	}
+	xdgVolumeInfo := TemplateXDGVolumeInfo(user.UserPrincipalName)
+
+	// just upload directly and shove it in the cache
+	// (since the fs isn't mounted yet)
+	resp, err := graph.Put(
+		graph.ResourcePath("/.xdg-volume-info")+":/content",
+		auth,
+		strings.NewReader(xdgVolumeInfo),
+	)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to write .xdg-volume-info")
+	}
+	root, _ := filesystem.GetPath("/", auth) // cannot fail
+	inode := fs.NewInode(".xdg-volume-info", 0644, root)
+	if json.Unmarshal(resp, &inode) == nil {
+		filesystem.InsertID(inode.ID(), inode)
+	}
 }
--- cmd/onedriver/main.go
+++ cmd/onedriver/main.go
@@ -149,7 +149,7 @@ func initializeFilesystem(config *common.Config, mountpoint string, authOnly, he
 		filesystem.StartCacheCleanup()
 	}
 
-	xdgVolumeInfo(filesystem, auth)
+	common.CreateXDGVolumeInfo(filesystem, auth)
 
 	// Sync the full directory tree if requested
 	if config.SyncTree {
@@ -245,33 +245,3 @@ func setupSignalHandler(filesystem *fs.Filesystem, server *fuse.Server) {
 	}()
 }
 
-// xdgVolumeInfo createx .xdg-volume-info for a nice little onedrive logo in the
-// corner of the mountpoint and shows the account name in the nautilus sidebar
-func xdgVolumeInfo(filesystem *fs.Filesystem, auth *graph.Auth) {
-	if child, _ := filesystem.GetPath("/.xdg-volume-info", auth); child != nil {
-		return
-	}
-	log.Info().Msg("Creating .xdg-volume-info")
-	user, err := graph.GetUser(auth)
-	if err != nil {
-		log.Error().Err(err).Msg("Could not create .xdg-volume-info")
-		return
-	}
-	xdgVolumeInfo := common.TemplateXDGVolumeInfo(user.UserPrincipalName)
-
-	// just upload directly and shove it in the cache
-	// (since the fs isn't mounted yet)
-	resp, err := graph.Put(
-		graph.ResourcePath("/.xdg-volume-info")+":/content",
-		auth,
-		strings.NewReader(xdgVolumeInfo),
-	)
-	if err != nil {
-		log.Error().Err(err).Msg("Failed to write .xdg-volume-info")
-	}
-	root, _ := filesystem.GetPath("/", auth) // cannot fail
-	inode := fs.NewInode(".xdg-volume-info", 0644, root)
-	if json.Unmarshal(resp, &inode) == nil {
-		filesystem.InsertID(inode.ID(), inode)
-	}
-}
