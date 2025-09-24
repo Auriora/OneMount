5ecf57827d4758fe65dc09dae85797c2b6584959
bcherrington <993834+bcherrington@users.noreply.github.com>
2025-09-24 13:25:32 +0100

    refactor: remove legacy pkg/ tree after migration to internal/

--- pkg/graph/mock/api_mock_graph.go
+++
@@ -1,7 +0,0 @@
-package mock
-
-// APIGraphProvider is an alias for MockGraphProvider for backward compatibility.
-type APIGraphProvider = MockGraphProvider
-
-// NewAPIGraphProvider is an alias for NewMockGraphProvider for backward compatibility.
-var NewAPIGraphProvider = NewMockGraphProvider
